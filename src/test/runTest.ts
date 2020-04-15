2c8f9d925a74e262f0fd77c82e62ddb76c7fdd5e
Mickey Pearce <mickey.pearce@mail.com>
2020-04-15 17:03:40 +0200

    add deps and tests with vscode-test

---
+++ src/test/runTest.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+
+import { runTests } from 'vscode-test';
+
+async function main() {
+	try {
+		// The folder containing the Extension Manifest package.json
+		// Passed to `--extensionDevelopmentPath`
+		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+
+		// The path to the extension test script
+		// Passed to --extensionTestsPath
+		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+
+		const testFolderPath = path.resolve(__dirname, '../../testFolder');
+
+		const launchArgs = [
+			testFolderPath,
+			// no other extension loaded
+			`--extensions-dir=${extensionDevelopmentPath}/.`
+		];
+
+		// Download VS Code, unzip it and run the integration test
+		await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs });
+	} catch (err) {
+		console.error('Failed to run tests');
+		process.exit(1);
+	}
+}
+
+main();
